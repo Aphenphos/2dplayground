f56c35d4340526225e23a01c197122029d23db8c
Aphenphos <maxwillwinters@gmail.com>
2023-11-24 02:46:26 -0500

    figured out a way to create my desired effect

--- main.c
+++ main.c
@@ -3,93 +3,90 @@
 #include <time.h>
 #include "SDL2/SDL.h"
 
-#define HEIGHT 480
-#define WIDTH 640
+#define HEIGHT 720
+#define WIDTH 1280
 #define ASPECT_RATIO 1.3334f
-#define ANIMATION_FPS 30
+#define ANIMATION_FPS 100
 #define MAXSIZE 25
+#define POINT_COUNT 100
 const float fpsInMS = 1.0f / ANIMATION_FPS;
 
-struct vec2d{
+struct vec2d {
     float x,y;
 };
 
 struct point {
-    struct vec2d pos;
+    struct vec2d pos, velocity;
+    struct vec2d trail[5];
     int color[3];
 };
 
-struct translate {
-    struct vec2d start, end, currentStartLoc, inc;
-    int currentStep;
-};
-static const struct translate emptyTranslate;
+struct point points[POINT_COUNT];
 
-struct point points[25];
-struct translate animationQueue[MAXSIZE];
-bool isFree[MAXSIZE] = {true};
 
 int randInRange(int lower, int upper) {
     return rand() % (upper - lower + 1) + lower;
 }
 
-void animate() {
-    for (int i=0; i < MAXSIZE; i++) {
-        if (isFree[i] == false) {
-            animationQueue[i].currentStartLoc.x += animationQueue[i].inc.x;
-            animationQueue[i].currentStartLoc.y += animationQueue[i].inc.y;
-            animationQueue[i].currentStep++;
-            
-            if (animationQueue[i].currentStep >= ANIMATION_FPS) {
-                animationQueue[i] = emptyTranslate;
-                isFree[i] = true;
-            }
+void animate(SDL_Renderer *renderer, double deltaTime) {
+    for (int i = 0; i < *(&points + 1) - points; i++) {
+        if (points[i].pos.x >= WIDTH) {
+            points[i].pos.x = WIDTH;
+            points[i].velocity.x = randInRange(-1,1) *.25f;
+        } else if (points[i].pos.x <= 1){
+            points[i].pos.x = 0;
+            points[i].velocity.x = randInRange(-1,1) *.25f; 
+        }
+        if (points[i].pos.y >= HEIGHT) {
+            points[i].pos.y = HEIGHT;
+            points[i].velocity.y = randInRange(-1,1) *.25f;
+        } else if (points[i].pos.y <= 1){
+            points[i].pos.y = 0;
+            points[i].velocity.y = randInRange(-1,1) *.25f; 
+        }
+        if (points[i].velocity.x == 0 || points[i].velocity.y == 0) {
+            points[i].velocity.x = randInRange(-1,1) *.25f; 
+            points[i].velocity.y = randInRange(-1,1) *.25f; 
         }
     }
-}
 
-void addTranslation(struct translate t) {
-    for (int i = 0; i < MAXSIZE; i++) {
-        if (isFree[i] == true) {
-            isFree[i] = false;
-            animationQueue[i] = t;
-            return;
-        }
-    }
 }
-void update(SDL_Window* window, SDL_Renderer *renderer, bool a, double deltaTime) {
+int count = 0;
+void update(SDL_Window* window, SDL_Renderer *renderer, SDL_Texture* trail, bool a, double deltaTime) {
+    count++;
     SDL_SetRenderDrawColor(renderer, 0,0,0,255);
     SDL_RenderClear(renderer);
+    if (a) {
+        animate(renderer, deltaTime);
+    }
+    
+    SDL_SetRenderTarget(renderer, NULL);
+    for (int i=0; i < *(&points+1) - points; i++) {
+        points[i].pos.x += points[i].velocity.x * deltaTime;
+        points[i].pos.y += points[i].velocity.y * deltaTime;
 
-    for (int i = 0; i < *(&points + 1) - points; i++) {
         SDL_SetRenderDrawColor(renderer, points[i].color[0], points[i].color[1], points[i].color[2], 255);
         SDL_RenderDrawPoint(renderer, points[i].pos.x,points[i].pos.y);
     }
-
-    if (a) {
-        animate();
-    }
-    SDL_SetRenderDrawColor(renderer, 0,0,255,255);
-    for (int i = 0; i < MAXSIZE; i++) {
-        if(isFree[i] == false) {
-            SDL_RenderDrawLine(renderer, animationQueue[i].currentStartLoc.x, animationQueue[i].currentStartLoc.y, animationQueue[i].end.x, animationQueue[i].end.y);
-        }
-    }
+    SDL_SetRenderTarget(renderer, trail);
+    SDL_RenderCopy(renderer, trail, NULL, NULL);
     SDL_RenderPresent(renderer);
+
     return;
 }
-
 
 void init(SDL_Window* window, SDL_Renderer *renderer) {
     SDL_SetRenderDrawColor(renderer, 0,0,0,255);
     SDL_RenderClear(renderer);
     srand(time(0));
     for (int i = 0; i < *(&points + 1) - points; i++) {
-        points[i].pos.x = randInRange(0,WIDTH);
-        points[i].pos.y = randInRange(0,HEIGHT);
+        points[i].pos.x =  WIDTH / 2;
+        points[i].pos.y = HEIGHT / 2;
+        points[i].velocity.x = randInRange(-1,1) *.25f;
+        points[i].velocity.y = randInRange(-1,1) *.25f;
         points[i].color[0] = points[i].color[1] = points[i].color[2] = 255;
         SDL_SetRenderDrawColor(renderer, points[i].color[0], points[i].color[1], points[i].color[2], 255);
-        SDL_RenderDrawPoint(renderer, points[i].pos.x,points[i].pos.y);
+        SDL_RenderDrawPoint(renderer, points[i].pos.x, points[i].pos.y);
     }
     SDL_RenderPresent(renderer);
 }
@@ -103,14 +100,15 @@ int main() {
 
     SDL_Window* window = SDL_CreateWindow("Atel2d", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
+    SDL_Texture *texture;
+    SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
     bool animate, secondPassed, isRunning;
     animate = secondPassed = isRunning = true;
     double deltaTime = 0;
     Uint64 prev = 0;
     Uint64 now = SDL_GetPerformanceCounter();
     init(window, renderer);
-    double timePassed, secondCounter;
+    double timePassed;
     while(isRunning) {
         SDL_Event event;
         while(SDL_PollEvent(&event)) {
@@ -124,31 +122,16 @@ int main() {
         now = SDL_GetPerformanceCounter();
 
         deltaTime = ((double)((now - prev)*1000 / (double)SDL_GetPerformanceFrequency()));
-        secondCounter += deltaTime;
         timePassed += deltaTime;
         animate = timePassed > (fpsInMS * 1000.0f);
-        secondPassed = secondCounter > 100.0f;
         if (animate) { timePassed = 0.0f;} 
-        if (secondPassed) {
-            for (int i=0; i < 25; i++) {
-                struct translate connection;
-                connection.start = points[randInRange(0,25)].pos;
-                connection.end = points[randInRange(0,25)].pos;
-                connection.inc.x = (connection.end.x - connection.start.x) * fpsInMS;
-                connection.inc.y = (connection.end.y - connection.start.y) * fpsInMS;
-                connection.currentStartLoc = connection.start;
-                connection.currentStep = 1;
-                addTranslation(connection);
-                connection.end = points[randInRange(0,25)].pos;
-                connection.inc.x = (connection.end.x - connection.start.x) * fpsInMS;
-                connection.inc.y = (connection.end.y - connection.start.y) * fpsInMS;
-                connection.currentStartLoc = connection.start;
-                connection.currentStep = 1;
-                addTranslation(connection);
-            }
-            secondCounter = 0.0f;
+        if (count >=100) {
+            SDL_DestroyTexture(texture);
+            texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, WIDTH, HEIGHT);
+            SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+            count = 0;
         }
-        update(window, renderer, animate, deltaTime);
+        update(window, renderer, texture, animate, deltaTime);
     }   
 
     SDL_DestroyRenderer(renderer);
